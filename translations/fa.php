3472bb4e05d38f86db77b6e0f00b4674ea765b65
Benichou <benoit.leforestier@gmail.com>
2017-09-19 14:50:19 +0200

    Add persian language

---
+++ translations/fa.php
@@ -0,0 +1,11 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_1d3c65b2b03ef35e14df6b163ea3a1f6'] = 'گوگل تگ منیجر';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_1c9989959823bdc5f68927bed4d52bf7'] = 'اضافه کردن اسکریپت گوگل تگ منیجر';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_462390017ab0938911d2d4e964c0cab7'] = 'تنظیمات با موفقیت بروزرسانی شد';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_de62775a71fc2bf7a13d7530ae24a7ed'] = 'تنظیمات همگانی';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_85c3d113ac1b88aa526b1119ac40bc98'] = 'شناسه Google Tag Manager';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_9f30b8386dc8bb95f4c0767b446177f6'] = 'شناسه خود را وارد کنید (GTM-XXXXXX).';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_c9cc8cce247e49bae79f15173ce97354'] = 'ذخیره';
