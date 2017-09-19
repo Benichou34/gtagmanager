c1adbd9b1b1d05d5135fca616c637d33e4ab4975
Benichou <benoit.leforestier@gmail.com>
2017-09-19 14:59:29 +0200

    Add french language

---
+++ translations/fr.php
@@ -0,0 +1,11 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_1d3c65b2b03ef35e14df6b163ea3a1f6'] = 'Google Tag Manager';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_1c9989959823bdc5f68927bed4d52bf7'] = 'Ajoute le script Google Tag Manager';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_462390017ab0938911d2d4e964c0cab7'] = 'Les paramètres ont été mis à jour avec succès';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_de62775a71fc2bf7a13d7530ae24a7ed'] = 'Paramètres';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_85c3d113ac1b88aa526b1119ac40bc98'] = 'Idendifiant Google Tag Manager';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_9f30b8386dc8bb95f4c0767b446177f6'] = 'Identifiant de votre compte (GTM-XXXXXX).';
+$_MODULE['<{gtagmanager}prestashop>gtagmanager_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
